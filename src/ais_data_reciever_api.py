b8f94260badbcf1b2369abe150d9594b5604d442
Benjamin Sundell <thorsundell@gmail.com>
2026-03-18 17:49:54 +0200

    changes

--- src/ais_data_reciever_api.py
+++ src/ais_data_reciever_api.py
@@ -1,17 +1,43 @@
 import paho.mqtt.client as mqtt
 import json
 import ssl
+import datetime as dt
+from datetime import timezone
+from fmiopendata.wfs import download_stored_query
 
-line = "----------"
+
+end_time = dt.datetime.now(timezone.utc)
+start_time = end_time - dt.timedelta(minutes=5)
+
+start_time = start_time.strftime("%Y-%m-%dT%H:%M:%SZ")
+end_time = end_time.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+def get_forecast(latlon):
+
+    forecast = download_stored_query(
+        "fmi::forecast::harmonie::surface::point::multipointcoverage",
+        args=["latlon=" + latlon,
+          "parameters=temperature,windspeedms,winddirection,humidity"]
+    )
+
+    current_forecast = min(forecast.data.keys())
+    current_grid = sorted(forecast.data[current_forecast].keys())[0]
+    print(forecast.data[current_forecast][current_grid].items())
+
+
 
 def on_connect(client, userdata, flags, rc):
-    client.subscribe("vessels-v2/230704000/location")
+    client.subscribe("vessels-v2/230703000/location")
     print("Connection established.")
 
 def on_message(client, userdata, msg):
     data = json.loads(msg.payload.decode())
-    print(json.dumps(data))
-    print(line * 15)
+    latitude = round(data["lat"], 2)
+    longitude = round(data["lon"], 2)
+    latlon = str(latitude) + ',' + str(longitude)
+
+    get_forecast(latlon)
+
 
 client = mqtt.Client(transport="websockets")
 client.tls_set(cert_reqs=ssl.CERT_REQUIRED)
@@ -20,3 +46,5 @@ client.on_message = on_message
 
 client.connect("meri.digitraffic.fi", 443)
 client.loop_forever()
+
+
